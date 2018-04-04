cef0a66d119774eba0c749b16a4e9e5097ca42e4
webconsole <webconsole@jrtk.net>
2018-04-04 19:24:12 +0800

    helloworld demo

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <phpcpp.h>
+#include <iostream>
+
+void say_hello()
+{
+	Php::out << "hello world from my first extension" << std::endl;
+}
+
+/**
+ *  tell the compiler that the get_module is a pure C function
+ */
+extern "C" {
+    
+    /**
+     *  Function that is called by PHP right after the PHP process
+     *  has started, and that returns an address of an internal PHP
+     *  strucure with all the details and features of your extension
+     *
+     *  @return void*   a pointer to an address that is understood by PHP
+     */
+    PHPCPP_EXPORT void *get_module() 
+    {
+        // static(!) Php::Extension object that should stay in memory
+        // for the entire duration of the process (that's why it's static)
+        static Php::Extension extension("helloworld", "1.0.0");
+        
+        // @todo    add your own functions, classes, namespaces to the extension
+		extension.add<say_hello>("say_hello");
+		
+        // return the extension
+        return extension;
+    }
+}
